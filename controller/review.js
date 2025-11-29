0b2bfe42d650987c309b2411a6b4b921361565c9
migarcos <migark@hotmail.com>
2025-11-29 11:17:15 -0500

    get Single, ByUser, Bydestination added

--- controller/review.js
+++ controller/review.js
@@ -13,4 +13,64 @@ const getAll = async (req, res) => {
     }
 };
 
-module.exports = { getAll };
+const getSingle = async (req, res) => {
+    //#swagger.tags=["Reviews"]
+    try {
+        const reviewId = new ObjectId(req.params.id);
+        const review = await mongodb.getDatabase().db('lucky7Travel').collection('review').findOne({ _id: reviewId });
+
+        if (!review) {
+            return res.status(404).json({ message: "Review not found!" })
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(review);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getByDestination = async (req, res) => {
+    //#swagger.tags=["Reviews"]
+    try {
+        const placeId = req.params.id;
+        const reviews = await mongodb.getDatabase()
+            .db('lucky7Travel')
+            .collection('review')
+            .find({ destnationId: placeId })
+            .toArray();
+
+        if (reviews.length === 0) {
+            return res.status(404).json({ message: "Review not found!" })
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(reviews);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getByUser = async (req, res) => {
+    //#swagger.tags=["Reviews"]
+    try {
+        const user = req.params.id;
+        const reviews = await mongodb.getDatabase()
+            .db('lucky7Travel')
+            .collection('review')
+            .find({ userId: user })
+            .toArray();
+
+        if (reviews.length === 0) {
+            return res.status(404).json({ message: "Review not found!" })
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(reviews);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+module.exports = { getAll, getSingle, getByDestination, getByUser };
--- routes/review.js
+++ routes/review.js
@@ -3,4 +3,8 @@ const reviewCtrl = require('../controller/review');
 
 router.get('/', reviewCtrl.getAll);
 
+router.get('/:id', reviewCtrl.getSingle);
+router.get('/getByDestination/:id', reviewCtrl.getByDestination);
+router.get('/getByUser/:id', reviewCtrl.getByUser);
+
 module.exports = router;
