938309d2a197eef555a11787602b1fdf718356db
jneberhard <jreberhard3@gmail.com>
2025-12-06 21:08:23 -0700

    updating post in users

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ const validateUser = require('../utilities/userValidation');
 const { isAuthenticated } = require('../utilities/userAuthentication');
 
 router.get('/', userController.getAll);
-
+router.post('/:id', validateUser, userController.createUser); // create a user
 router.get('/:id', userController.getSingleUser); // getting a single user by ID
+
 router.put('/:id', isAuthenticated, validateUser, userController.updateUser);  //update User
 router.delete('/:id', isAuthenticated, userController.deleteUser);   //delete User
 
